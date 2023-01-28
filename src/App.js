0687f7eaffbbf7fd73b17dde20bfffdc7856449d
RAJESH KUMAR SAHOO <rajeshkumarsahoo006@gmail.com>
2023-01-28 11:52:57 +0530

    FrontEnd

---
+++ src/App.js
@@ -0,0 +1,57 @@
+// import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Components/home'
+import ErrorPage from './Components/errorPage'
+import SignUp from './Components/signup';
+import Login from './Components/login';
+import Book from './Components/bookPage'
+import AddBook from './Components/addBook';
+import UpdateBook from './Components/updateBook';
+
+
+function App() {
+  return (
+
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+
+      <BrowserRouter>
+        <div className='logo'><h2>UW Infotech</h2></div>
+        <div className='route'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+
+            <Route path='signup' element={<SignUp />} />
+
+            <Route path='login' element={<Login />} />
+
+            <Route path='getBook' element={<Book />} />
+            
+            <Route path='addBook' element={<AddBook />} />
+            
+            <Route path='updateBook/:id' element={<UpdateBook />} />
+
+            <Route path='*' element={<ErrorPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
